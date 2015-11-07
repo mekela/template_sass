9d5f0b18c57e8a81bfbe6d84a7a008fa0b66f384
alex <sania_free@i.ua>
2015-11-08 00:41:51 +0200

    First version
    This is first version of basic sass template.

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	//fancybox
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	
+	//placeholder
+	$('input[placeholder], textarea[placeholder]').placeholder();
+});
